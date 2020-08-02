8633626953078bd4fe4a6060d02e81b3c8625191
arief irawan <ariefirawant@gmail.com>
2020-08-02 12:48:09 +0700

    firestore hook and showing image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Title from './Components/Title';
 import UploadForm from './Components/UploadForm';
+import ImageGrid from './Components/ImageGrid';
 
 function App() {
   return (
     <div className="App">
       <Title />
       <UploadForm />
+      <ImageGrid />
     </div>
   );
 }
